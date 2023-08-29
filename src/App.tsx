9f93d70a8301610dbdddecf1f7c368f11aa605f0
maxhha <devmaxhha@gmail.com>
2023-08-29 23:24:15 +0300

    Layout for projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    setGreetMsg(await invoke("create_project", { name }));
-  }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="page">
+      <h1 className="page__header">Projects</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <pre>{JSON.stringify(greetMsg, null, 2)}</pre>
+      <button className="button">+New</button>
+      <ul className="projects">
+        {["Project 1", "Second Project", "My active project"].map((name, i) => (
+          <li className="project" tabIndex={0}>
+            <span className="project__name">{name}</span>{' '}
+            <span className="project__id">#{i}</span>{' '}
+            <span className="project__last-update">-- 3 minutes ago</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
